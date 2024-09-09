e74d3f943121b2ae52126047b81476fab66e0c44
Hantha <69382830+hanthap@users.noreply.github.com>
2024-09-09 12:03:42 +1000

    Initial coding

---
+++ Export-Accdb.ps1
@@ -0,0 +1,76 @@
+﻿
+function Append-Accdb {
+
+param( 
+    [parameter(mandatory=$true)] [string] $Path,
+    [Parameter(ValueFromPipeline)] [psobject] $o
+)
+
+begin {
+
+    $cn = New-Object System.Data.OleDb.OleDbConnection
+    $cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=$Path;Mode=Share Deny None;"
+    [void] $cn.Open() 
+
+    $cmd = New-Object System.Data.OleDb.OleDbCommand
+    $cmd.Connection = $cn
+
+    $parm1 = New-Object System.Data.OleDb.OleDbParameter
+    $parm2 = New-Object System.Data.OleDb.OleDbParameter
+    $parm3 = New-Object System.Data.OleDb.OleDbParameter
+
+    [void] $cmd.Parameters.Add( $parm1 )
+    [void] $cmd.Parameters.Add( $parm2 )
+    [void] $cmd.Parameters.Add( $parm3 )
+
+    $sql = "insert into Case_Note__c ( Id, Notes__c, Action_Detail__c ) values ( ?, ?, ? )"
+    $cmd.CommandText = $sql
+
+}
+
+process {
+
+    $parm1.Value = $o.Id
+    # skip null bytes
+    try { $parm2.Value = $o.Notes__c.Substring(1) } catch { $parm2.Value = '' } 
+    try { $parm3.Value = $o.Action_Detail__c.Substring(1) } catch { $parm3.Value = '' } 
+
+    [void] $cmd.ExecuteNonQuery()  #returns number of rows
+
+}
+
+end {
+    $cn.Close()
+    }
+
+}
+
+$csvPath = 'C:\Users\PeterLuckock\Downloads\Case_Note__c.csv'
+$accdbPath = 'C:\Users\PeterLuckock\Downloads\test.accdb'
+
+Get-Content $csvPath -TotalCount 1000 | 
+    ConvertFrom-Csv | 
+        Append-Accdb -Path $accdbPath
+
+<#
+
+$fields = {
+“Date” = “06/19/2014”;
+“Name” = “Nicolas1847”
+“Spiceworks” = “Rulez”
+}
+
+
+process {
+foreach ($field in $fields.keys)
+{
+$recordset.Fields.item($field) = $($fields.$field)
+}
+}
+
+
+end {
+
+}
+
+#>
---
+++ Import Case Notes.ps1
@@ -0,0 +1,74 @@
+﻿<#
+$f = 'C:\Users\PeterLuckock\Downloads\Case_Note__c.csv'
+
+
+$indata = Get-Content $f -TotalCount 2 | ConvertFrom-Csv 
+
+$indata
+
+#----------------------------- ##>
+
+    
+function Import-Accdb {
+param( 
+    [parameter(mandatory=$true)] [string] $ViewName,
+    [parameter(mandatory=$true)] [string] $Path,
+    $SQL = "select * from $ViewName",
+    $CountSQL = "select count(*) as N from ( $SQL )",
+    $Caption = "Processing $ViewName in $Path..."
+)
+
+begin {
+
+    $cn = New-Object System.Data.OleDb.OleDbConnection
+    $cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=$Path;Mode=Share Deny Write;"
+    [void] $cn.Open() # seems to work here inside function, but not in open code
+
+    $cmd = New-Object System.Data.OleDb.OleDbCommand
+    $cmd.Connection = $cn
+
+    if ( $CountSQL ) {
+        try {
+        Write-Host "Checking batch size"
+        $cmd.CommandText = $countSQL
+        $rs = $cmd.ExecuteReader()
+        [void]$rs.Read()
+        $nTotalCount = $rs.GetValue(0)
+        Write-Host "Total batch count $nTotalCount"
+        } catch { }
+    }
+
+    $rs.Close()
+
+    $cmd.CommandText = $sql
+    $rs = $cmd.ExecuteReader()
+    $nDocsProcessed = 0
+    Write-Progress -PercentComplete 0 -Activity $caption -Status "Processed $nDocsProcessed of $nTotalCount"
+
+    while ( $rs.Read() ) {
+        $ht = @{}
+        for ( $i=0; $i -lt $rs.FieldCount; $i++ )
+        {
+            if ( $rs.GetValue($i) -ne $null ) { $ht.Add( $rs.GetName($i), $rs.GetValue($i) ) }
+        } 
+        [pscustomobject] $ht
+
+        $nDocsProcessed++
+        $pctDone = $nDocsProcessed / $nTotalCount * 100
+        Write-Progress -PercentComplete $pctDone -Activity $caption -Status "Processed $nDocsProcessed of $nTotalCount."
+        }
+    $rs.Close()
+
+    $cn.Close()
+
+    }  # end begin
+
+
+} # end function
+
+
+$accdb = 'C:\Users\PeterLuckock\Downloads\test.accdb'
+
+$d = Import-Accdb -ViewName Campaign -Path $accdb 
+
+$d 
